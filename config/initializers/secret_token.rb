80063d74d0cf0e522a278a9273f7a9ec825cbc98
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-03-20 20:33:29 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FruitsVeggies::Application.config.secret_key_base = 'b1b85285576d3f692ae3f1859928941d52c50f8287ab04dd74b1f4f646f69b149d465e6e3f16f215ffdad2878489433e5534c1b023eab31359322f23869551c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FruitsVeggies::Application.config.session_store :cookie_store, key: '_fruits_veggies_session'
