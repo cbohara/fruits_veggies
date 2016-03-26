9daf6d3b3c7d54b17fddfec52392e0defd936e15
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-03-26 23:09:08 +0000

    finishing touches

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -8,4 +8,15 @@ class FruitsController < ApplicationController
     @fruit = Fruit.new
   end
 
+  def create
+    Fruit.create(fruit_params)
+    redirect_to root_path
+  end
+
+  private 
+
+  def fruit_params
+    params.require(:fruit).permit(:name, :fruitorveggie, :benefits)
+  end
+
 end
